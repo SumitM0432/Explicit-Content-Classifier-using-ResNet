da8f585e4363843009d3e5a827850db000e42e37
Sumit Mishra <46062583+SumitM0432@users.noreply.github.com>
2021-09-11 00:20:15 +0530

    Code to split the images into train and test
    Minor Changes

--- Split.py
+++ Split.py
@@ -1,13 +1,13 @@
 import os
 import random
 import shutil
-def split_data(category):
+def split_data(category, file_path):
 
-    source = 'C:\\Users\sumit\\Desktop\\Explicit Content Classifier\\Input\\train\\' + category
-    filenames = os.listdir('C:\\Users\sumit\\Desktop\\Explicit Content Classifier\\Input\\train\\' + category)
-    # print (type(filenames))
+    source = file_path + 'train\' + category
+    filenames = os.listdir(file_path + 'train\' + category)
+    
     print (len(filenames))
-    print (filenames[0:10])
+    print ('First Ten Files: ', filenames[0:10])
 
     filenames.sort()
 
@@ -18,16 +18,10 @@ def split_data(category):
     test_filenames = filenames[:split]
     print (len(test_filenames))
 
-    destination = 'C:\\Users\\sumit\\Desktop\\Explicit Content Classifier\\Input\\test\\' + category
+    destination = file_path + 'test\' + category
 
     for name in test_filenames:
         ini_path = os.path.join(source, name)
         fin_path = os.path.join(destination, name)
 
         shutil.move(ini_path, fin_path)
-
-# split_data('Hentai')
-# split_data('Neutral')
-# split_data('Porn')
-# split_data('Drawing')
-# split_data('Sexy')
