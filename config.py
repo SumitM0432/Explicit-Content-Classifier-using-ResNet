56d81f288112c31f9023eedcd5085d985914cc0e
Sumit Mishra <46062583+SumitM0432@users.noreply.github.com>
2021-09-11 00:22:57 +0530

    Configuration File

--- config.py
+++ config.py
@@ -6,10 +6,8 @@ np.random.seed(300)
 BATCH_SIZE = 32
 EPOCHS = 5
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-TRAINING_FILE = '/media/levi/OS/Users/sumit/Desktop/Explicit Content Classifier/Input/train/'
-TESTING_FILE = '/media/levi/OS/Users/sumit/Desktop/Explicit Content Classifier/Input/test/'
-# TRAINING_FILE = 'C:\\Users\\sumit\\Desktop\\Explicit Content Classifier\\Input\\train'
-# VALIDATION_FILE = 'C:\\Users\\sumit\\Desktop\\Explicit Content Classifier\\Input\\test'
-OUT = '/media/levi/OS/Users/sumit/Desktop/Explicit Content Classifier/Output/'
+TRAINING_FILE = '../Input/train/'
+TESTING_FILE = '../Input/test/'
+OUT = '../Output/'
 LEARNING_RATE = 0.0001
 N_CLASSES = 5
